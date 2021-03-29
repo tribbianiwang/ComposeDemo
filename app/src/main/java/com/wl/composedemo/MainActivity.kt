818ff4454e42c015cf8419570ebd28b4ea0956f9
tribbianiwang <1130455004@qq.com>
2021-03-29 15:31:39 +0800

    column

--- app/src/main/java/com/wl/composedemo/MainActivity.kt
+++ app/src/main/java/com/wl/composedemo/MainActivity.kt
@@ -3,31 +3,55 @@ package com.wl.composedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wl.composedemo.ui.theme.ComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "wdl")
+//            Greeting(name = "wdl")
+            NewStory()
         }
     }
-    
-    
+
+
     @Composable
-    fun Greeting(name:String){
-        Text("hello ${name}")
+    fun NewStory(){
+        Column(modifier = Modifier.padding(16.dp)){
+            Text("郑羽君")
+            Text("电话:13223077009")
+            Text("网站:www.will.org.cn")
+        }
+
     }
+
 
     @Preview
     @Composable
-    fun PreviewGreeting(){
-        Greeting(name = "dc")
+    fun PreviewNewStory(){
+        NewStory()
     }
+
+    
+    
+//    @Composable
+//    fun Greeting(name:String){
+//        Text("hello ${name}")
+//    }
+//
+//    @Preview
+//    @Composable
+//    fun PreviewGreeting(){
+//        Greeting(name = "dc")
+//    }
 }
 

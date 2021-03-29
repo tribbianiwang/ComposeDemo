0cb7182d02143fb2f3d83267199507ac3b7c4b48
tribbianiwang <1130455004@qq.com>
2021-03-29 15:48:02 +0800

    图片，参数

--- app/src/main/java/com/wl/composedemo/MainActivity.kt
+++ app/src/main/java/com/wl/composedemo/MainActivity.kt
@@ -3,13 +3,16 @@ package com.wl.composedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.wl.composedemo.ui.theme.ComposeDemoTheme
@@ -27,6 +30,13 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun NewStory(){
         Column(modifier = Modifier.padding(16.dp)){
+            Image(painter = painterResource(R.drawable.header), contentDescription = null,
+                modifier = Modifier.height(180.dp).fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(Modifier.height(16.dp))
+
             Text("郑羽君")
             Text("电话:13223077009")
             Text("网站:www.will.org.cn")
